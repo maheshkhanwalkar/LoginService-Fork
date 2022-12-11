395227bb9d4eaedccb22aa9bf08c0da9c32ff20a
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2022-12-11 16:47:03 -0500

    Fix other issues

--- User/User/src/main/java/com/User/SecurityConfiguration.java
+++ User/User/src/main/java/com/User/SecurityConfiguration.java
@@ -31,9 +31,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/", "/login**").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .oauth2Login()
-                .and().antMatcher("/oauth2/authorization/github")
-                .cors().configurationSource(corsConfigurationSource());
+                .oauth2Login();
+                //.and().antMatcher("/oauth2/authorization/github")
+                //.cors().configurationSource(corsConfigurationSource());
     }
 
     @Bean
