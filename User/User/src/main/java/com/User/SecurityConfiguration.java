3738b12db696bf7535210a69ad1cc13fb6bb7623
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2022-12-11 17:48:42 -0500

    Fix other issues

--- User/User/src/main/java/com/User/SecurityConfiguration.java
+++ User/User/src/main/java/com/User/SecurityConfiguration.java
@@ -28,13 +28,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.antMatcher("/**")
+                .cors().configurationSource(corsConfigurationSource())
+                .and()
                 .authorizeRequests()
                 .antMatchers("/", "/login**").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .oauth2Login()
-                .and()
-                .cors().configurationSource(corsConfigurationSource());
+                .oauth2Login();
     }
 
     @Bean
